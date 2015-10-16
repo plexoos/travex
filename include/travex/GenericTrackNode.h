3082f713d1ae41d385b0b5c1d2d62f9a7b0207a3
Dmitri Smirnov <d.s@plexoos.com>
2015-10-16 17:38:42 -0400

    GenericTrackNode: Introduced interface and cleaned up the specialization

---
+++ include/travex/GenericTrackNode.h
@@ -0,0 +1,41 @@
+#ifndef tvx_GenericTrackNode_h
+#define tvx_GenericTrackNode_h
+
+
+namespace tvx {
+
+
+/**
+ * A templated abstract base class interface with a basic functionality common
+ * to all track nodes identified by a tracking algorithm.
+ *
+ * \todo The type (Coordinates_t) for Errors should be renamed of course
+ */
+template<class _Coordinates, class _TrackNodeHitContainer>
+class GenericTrackNode
+{
+public:
+
+   using Coordinates_t = _Coordinates;
+   using TrackNodeHitContainer_t = _TrackNodeHitContainer;
+   using Hit_t = typename _TrackNodeHitContainer::value_type;
+
+   virtual ~GenericTrackNode() {}
+
+   virtual const Coordinates_t& GetPosition()  const = 0;
+   virtual const Coordinates_t& GetMomentum()  const = 0;
+   virtual const Coordinates_t& GetError()     const = 0;
+   virtual const Coordinates_t& GetProjError() const = 0;
+   virtual const Hit_t* GetAcceptedHit() const = 0;
+   virtual const TrackNodeHitContainer_t& GetCandidateHits() const = 0;
+
+   friend bool operator< (const GenericTrackNode& lhs, const GenericTrackNode& rhs)
+   {
+      return lhs.GetPosition() < rhs.GetPosition();
+   }
+};
+
+
+}
+
+#endif
--- include/travex/TrackNode.h
+++ include/travex/TrackNode.h
@@ -8,17 +8,18 @@
 #include "TVector3.h"
 
 #include "travex/Hit.h"
-#include "travex/HitProxy.h"
+#include "travex/GenericTrackNode.h"
 
 
 namespace tvx {
 
-class Track;
 
 
-class TrackNode : public TObject
+class TrackNode : public GenericTrackNode<TVector3, std::set<Hit> >, public TObject
 {
 public:
+
+   using Hit_t = typename TrackNodeHitContainer_t::value_type;
 
    TrackNode();
 
@@ -26,47 +27,21 @@ public:
    const TVector3& GetMomentum()  const { return fMomentum; }
    const TVector3& GetError()     const { return fError; }
    const TVector3& GetProjError() const { return fProjError; }
-   const Hit* GetAcceptedHit() const { return fAcceptedHit; }
-   const Hit* GetClosestHit() const { return fClosestHit; }
-   const std::set<HitProxy>& GetCandidateHits() const { return fCandidateHits; }
-   std::set<const Hit*> GetCandidateBareHits() const;
-   void SetClosestHit(const std::set<Hit>& hits) const;
-   void FindCandidateHits(const std::set<Hit>& hits) const;
+   const Hit_t* GetAcceptedHit() const { return fAcceptedHit; }
+   const TrackNodeHitContainer_t& GetCandidateHits() const { return fCandidateHits; }
 
-   double   CalcChi2(const Hit& hit) const { return -1; }
-   TVector3 CalcDiffProjToFitError() const { return fProjError - fError; }
-
-   double CalcDistanceToClosestHit() const
-   {
-      return fClosestHit ? (fClosestHit->GetPosition() - GetPosition()).Mag() : -1;
-   }
-
-   double CalcDistanceToHit() const
-   {
-      return fAcceptedHit ? (fAcceptedHit->GetPosition() - GetPosition()).Mag() : -1;
-   }
-
-   double CalcDistanceToHit(const Hit& hit) const
-   {
-      return (hit.GetPosition() - GetPosition()).Mag();
-   }
-
-   TVector3 CalcPullToHit(const Hit& hit) const;
-   TVector3 CalcPullClosestHit() const;
    virtual void Print(Option_t *opt = "") const;
 
    friend bool operator< (const TrackNode& lhs, const TrackNode& rhs);
 
 protected:
 
-   const Track*  fTrack;                        //!< Transient pointer to parent track containing this node
-   TVector3      fPosition;                     ///< Coordinates of the track state/node position
-   TVector3      fMomentum;                     ///< Track momentum vector in global CS
-   TVector3      fError;                        ///< Diagonal elements of error matrix after final fit
-   TVector3      fProjError;                    ///< The projection error to the node before the fit
-   mutable const Hit* fAcceptedHit;             ///< Pointer to the hit associated with this node by the reconstruction algorithm, if any
-   mutable const Hit* fClosestHit;              ///< Pointer to the hit closest to this node if any
-   mutable std::set<HitProxy> fCandidateHits;   ///< Collection of hits in some proximity of mean track projection
+   TVector3      fPosition;          ///< Coordinates of the track state/node position
+   TVector3      fMomentum;          ///< Track momentum vector in global CS
+   TVector3      fError;             ///< Diagonal elements of error matrix after final fit
+   TVector3      fProjError;         ///< The projection error to the node before the fit
+   const Hit_t*  fAcceptedHit;       ///< Pointer to the hit associated with this node by the reconstruction algorithm, if any
+   TrackNodeHitContainer_t fCandidateHits;   ///< Collection of hits in some proximity of mean track projection
 
    ClassDef(TrackNode, 1)
 };
