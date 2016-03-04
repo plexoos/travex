d5eaaad8dd005b0d8394978989cdb015bd67c09b
Dmitri Smirnov <d.s@plexoos.com>
2016-03-04 10:33:26 -0500

    utils.h: Revised simple macros wrapping ROOT's logging functions

--- include/travex/utils.h
+++ include/travex/utils.h
@@ -1,21 +1,25 @@
 #ifndef tvx_utils_h
 #define tvx_utils_h
 
-#include "TROOT.h"
+#include "TError.h"
 
-//namespace tvx {
+namespace tvx {
 
 //void Info(const char *method, const char *msgfmt, ...) const
 //{
 //   gROOT->Info();
 //}
 
-#define Info(msgfmt, ...) gROOT->Info(__FUNCTION__, msgfmt, __VA_ARGS__)
+#define Info(...)     Info(__PRETTY_FUNCTION__, __VA_ARGS__)
+#define Warning(...)  Warning(__PRETTY_FUNCTION__, __VA_ARGS__)
+#define Error(...)    Error(__PRETTY_FUNCTION__, __VA_ARGS__)
+#define Fatal(...)    Fatal(__PRETTY_FUNCTION__, __VA_ARGS__)
+
 //char str[100];\
 //sprintf(str, __VA_ARGS__);\
 //std::cout << "[" << __FILE__ << "][" << __FUNCTION__ << "][Line " << __LINE__ << "] " << str << std::endl;\
 
 
-//}
+}
 
 #endif
