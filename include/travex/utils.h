09d7373a55238ac5ae8c351b315428780fb2fa10
Dmitri Smirnov <d.s@plexoos.com>
2016-07-05 16:55:27 -0400

    utils: Use portable c++ macro for current function name

--- include/travex/utils.h
+++ include/travex/utils.h
@@ -4,10 +4,10 @@
 #include "TError.h"
 
 
-#define TVX_INFO(...)     ::Info(__PRETTY_FUNCTION__, __VA_ARGS__)
-#define TVX_WARNING(...)  ::Warning(__PRETTY_FUNCTION__, __VA_ARGS__)
-#define TVX_ERROR(...)    ::Error(__PRETTY_FUNCTION__, __VA_ARGS__)
-#define TVX_FATAL(...)    ::Fatal(__PRETTY_FUNCTION__, __VA_ARGS__)
+#define TVX_INFO(...)     ::Info(__func__, __VA_ARGS__)
+#define TVX_WARNING(...)  ::Warning(__func__, __VA_ARGS__)
+#define TVX_ERROR(...)    ::Error(__func__, __VA_ARGS__)
+#define TVX_FATAL(...)    ::Fatal(__func__, __VA_ARGS__)
 
 
 #endif
